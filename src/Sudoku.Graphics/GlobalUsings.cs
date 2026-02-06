2c60bca0c57bd711e938f91e7cb938a7eae7c144
Kyouyama Kazusa <kazusa0805@outlook.com>
2026-02-06 23:51:01 +0800

    Update code.

--- src/Sudoku.Graphics/GlobalUsings.cs
+++ src/Sudoku.Graphics/GlobalUsings.cs
@@ -3,4 +3,5 @@ global using System.Diagnostics.CodeAnalysis;
 global using System.IO;
 global using System.Numerics;
 global using System.Runtime.CompilerServices;
+global using System.Text.Json.Serialization;
 global using SkiaSharp;
---
+++ src/Sudoku.Graphics/Graphics/CanvasDrawingOptions.cs
@@ -0,0 +1,23 @@
+﻿namespace Sudoku.Graphics;
+
+/// <summary>
+/// Represents drawing options.
+/// </summary>
+public sealed class CanvasDrawingOptions : IOptionsProvider<CanvasDrawingOptions>
+{
+	/// <summary>
+	/// Indicates the default instance.
+	/// </summary>
+	public static readonly CanvasDrawingOptions Default = new();
+
+
+	/// <summary>
+	/// Represents background color. By default it's equivalent value of color <see cref="SKColors.White"/>.
+	/// </summary>
+	/// <seealso cref="SKColors.White"/>
+	public SerializableColor BackgroundColor { get; set; } = SKColors.White;
+
+
+	/// <inheritdoc/>
+	static CanvasDrawingOptions IOptionsProvider<CanvasDrawingOptions>.DefaultInstance => Default;
+}
