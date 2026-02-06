abe5f0c806ac3fa6d08b89cc14dc52ba5caf53d5
Kyouyama Kazusa <Kazusa0805@outlook.com>
2026-02-06 19:53:39 +0800

    Test.

--- Sudoku.Graphics.ApiTesterConsole/Program.cs
+++ Sudoku.Graphics.ApiTesterConsole/Program.cs
@@ -6,4 +6,3 @@ using Sudoku.Graphics;
 using var canvas = new Canvas(40, 10, 9, 9);
 canvas.Clear(SKColors.White);
 canvas.Export(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "output.png"), new() { Quality = 100 });
-Console.WriteLine("Okay.");
