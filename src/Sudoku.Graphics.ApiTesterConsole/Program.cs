a8eecf6b8f42cde74f57e3df9aa69ecb737455cf
Kyouyama Kazusa <kazusa0805@outlook.com>
2026-02-06 22:55:45 +0800

    Add comment.

--- src/Sudoku.Graphics.ApiTesterConsole/Program.cs
+++ src/Sudoku.Graphics.ApiTesterConsole/Program.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿// This project only test for APIs.
+//
+// I don't usually use NUnit or XUnit test-related packages,
+// so I just use a console project to test for them.
+//
+// If you don't want to view for this, you can remove this project,
+// or just close this project from solution explorer :) 
+
+using System;
 using System.IO;
 using SkiaSharp;
 using Sudoku.Graphics;
@@ -7,4 +15,4 @@ var desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
 using var canvas = new Canvas(120, 10, 9, 9);
 canvas.Clear(SKColors.White);
 canvas.Export(Path.Combine(desktop, "output.png"), new() { Quality = 100 });
-Console.WriteLine("okay.");
+Console.WriteLine("Okay.");
