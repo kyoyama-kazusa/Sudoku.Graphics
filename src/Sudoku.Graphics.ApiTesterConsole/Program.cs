b03d8adf4b5e7f8896e3a01139e1b99ffd78f866
Kyouyama Kazusa <kazusa0805@outlook.com>
2026-02-06 22:37:53 +0800

    Move files.

---
+++ src/Sudoku.Graphics.ApiTesterConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.IO;
+using SkiaSharp;
+using Sudoku.Graphics;
+
+var desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+using var canvas = new Canvas(120, 10, 9, 9);
+canvas.Clear(SKColors.White);
+canvas.Export(Path.Combine(desktop, "output.png"), new() { Quality = 100 });
+Console.WriteLine("okay.");
