74655a019a1d6aaf47aa4d353400964cc3cd66f9
Kyouyama Kazusa <Kazusa0805@outlook.com>
2026-02-09 14:37:11 +0800

    Update code.

--- src/Sudoku.Graphics/GlobalUsings.cs
+++ src/Sudoku.Graphics/GlobalUsings.cs
@@ -1,4 +1,7 @@
 ﻿global using System;
 global using System.Diagnostics.CodeAnalysis;
 global using System.IO;
+global using System.Numerics;
+global using System.Runtime.CompilerServices;
+global using System.Text.Json.Serialization;
 global using SkiaSharp;
