ae04c23b514929b4b1393042566ce733c8b93e6b
Kyouyama Kazusa <Kazusa0805@outlook.com>
2026-02-09 14:26:39 +0800

    Update code.

--- Sudoku.Graphics/Concepts/Cell.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Sudoku.Concepts;
-
-/// <summary>
-/// Represents a cell coordinate. The cell will be designed inside a grid. If for a puzzle type that will display outside labels,
-/// the value can be negative or greater than logical count of rows and columns.
-/// </summary>
-/// <param name="Row">Indicates the row index.</param>
-/// <param name="Column">Indicates the column index.</param>
-public readonly record struct Cell(int Row, int Column) : IEqualityOperators<Cell, Cell, bool>;
