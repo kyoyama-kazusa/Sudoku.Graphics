72c2c9d5119eec60f3e3e8c6cf53179807502e80
Kyouyama Kazusa <kazusa0805@outlook.com>
2026-02-16 20:08:48 +0800

    Update code.

--- src/Sudoku.Graphics/Graphics/Primitives/IOptionsProvider.cs
+++ src/Sudoku.Graphics/Graphics/Primitives/IOptionsProvider.cs
@@ -8,6 +8,19 @@ public interface IOptionsProvider<out TSelf> : IOptionsSupportsSerialization<TSe
 	where TSelf : notnull, IOptionsProvider<TSelf>, new()
 {
 	/// <summary>
+	/// Represents serializer options.
+	/// </summary>
+	protected static readonly JsonSerializerOptions SerializerOptions = new()
+	{
+		WriteIndented = true,
+		IndentCharacter = ' ',
+		IndentSize = 2,
+		AllowTrailingCommas = false,
+		Converters = { new JsonStringEnumConverter() }
+	};
+
+
+	/// <summary>
 	/// Indicates the default options instance.
 	/// </summary>
 	static virtual TSelf DefaultInstance { get; } = new();
