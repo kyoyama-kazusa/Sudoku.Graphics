06041fab54ae5c6add714abf729d15d4f1eade2d
Kyouyama Kazusa <kazusa0805@outlook.com>
2026-02-18 11:21:44 +0800

    Update doc comments.

--- src/Sudoku.Graphics/Resources/ResourceKeyAttribute.cs
+++ src/Sudoku.Graphics/Resources/ResourceKeyAttribute.cs
@@ -4,7 +4,10 @@
 /// Indicates the property resource key.
 /// Such key configured will be called in UI projects (reflection) to get target information on I18N-related scenarios.
 /// </summary>
-/// <param name="key">The key.</param>
+/// <param name="key">
+/// <para><inheritdoc cref="Key" path="/summary"/></para>
+/// <para><b>Do not use <see langword="nameof"/> expressions here in order to prevent renaming and mismatching cases.</b></para>
+/// </param>
 [AttributeUsage(AttributeTargets.Property, Inherited = false)]
 public sealed class ResourceKeyAttribute(string key) : Attribute
 {
