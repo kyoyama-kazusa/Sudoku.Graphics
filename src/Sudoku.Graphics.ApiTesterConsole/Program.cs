1efa17d9bb0048f452b5803714e59a98b01d1e3d
Kyouyama Kazusa <kazusa0805@outlook.com>
2026-02-21 16:59:50 +0800

    Update code.

--- src/Sudoku.Graphics.ApiTesterConsole/Program.cs
+++ src/Sudoku.Graphics.ApiTesterConsole/Program.cs
@@ -8,9 +8,6 @@
 
 using System;
 using System.IO;
-using System.Linq;
-using System.Text.Json;
-using SkiaSharp;
 using Sudoku.ComponentModel;
 using Sudoku.ComponentModel.GridTemplates;
 using Sudoku.ComponentModel.Items;
@@ -19,44 +16,7 @@ using Sudoku.Graphics;
 var desktop = Environment.DesktopPath;
 var defaultTemplateSize = new GridTemplateSize(9, 9);
 var baseMapper = new PointMapper(120, 15, defaultTemplateSize);
-using var canvas = new Canvas(
-	[
-		new StandardGridTemplate { Mapper = baseMapper },
-		//new SpecifiedGridTemplate
-		//{
-		//	ThickLineSegments = [
-		//		.. Piece.O.GetOutline(RotationType.None, baseMapper, 10, 0),
-		//		.. Piece.T.GetOutline(RotationType.None, baseMapper, 10, 3),
-		//		.. Piece.J.GetOutline(RotationType.None, baseMapper, 10, 6),
-		//		.. Piece.L.GetOutline(RotationType.None, baseMapper, 13, 0),
-		//		.. Piece.S.GetOutline(RotationType.None, baseMapper, 13, 3),
-		//		.. Piece.Z.GetOutline(RotationType.None, baseMapper, 13, 6)
-		//	],
-		//	ThinLineSegments = [],
-		//	Mapper = baseMapper
-		//}
-		//new JigsawGridTemplate
-		//{
-		//	CellIndexGroups = [
-		//		[0, 1, 2, 9, 10, 11, 12, 21, 22],
-		//		[3, 4, 5, 6, 7, 13, 14, 15, 16],
-		//		[8, 17, 23, 24, 25, 26, 31, 32, 34],
-		//		[18, 19, 20, 27, 36, 45, 54, 55, 56],
-		//		[28, 29, 30, 37, 38, 39, 46, 47, 48],
-		//		[33, 35, 40, 41, 42, 43, 44, 51, 53],
-		//		[57, 58, 63, 64, 65, 66, 72, 73, 74],
-		//		[49, 50, 52, 59, 60, 61, 62, 71, 80],
-		//		[67, 68, 69, 70, 75, 76, 77, 78, 79]
-		//	],
-		//	Mapper = baseMapper,
-		//	//AlsoFillGroups = true
-		//}
-		//new SujikenGridTemplate { Mapper = baseMapper }
-		//new DefaultGridTemplate { Mapper = baseMapper }
-	]
-);
-
-var gridTextItems = JsonSerializer.Deserialize<Item[]>(GridJsonString, Options)!;
+using var canvas = new Canvas([new StandardGridTemplate { Mapper = baseMapper }]);
 canvas.DrawItems(
 	[
 		new BackgroundFillItem { Color = canvas.GetOptionValue(static options => options.BackgroundColor) },
@@ -64,16 +24,6 @@ canvas.DrawItems(
 		{
 			FillIntersectionCells = true,
 			TemplateIntersectionCellsColor = canvas.GetOptionValue(static options => options.TemplateIntersectionColor)
-		},
-		.. gridTextItems,
-		..
-		from GivenTextItem item in gridTextItems
-		let digit = int.Parse(item.Text) - 1
-		select new CandidateFillItem
-		{
-			TemplateIndex = 0,
-			CandidatePosition = new(item.Cell, 3, digit),
-			Color = SKColors.Yellow
 		}
 	]
 );
@@ -88,197 +38,16 @@ Console.WriteLine("Okay.");
 /// </summary>
 file static partial class Program
 {
-	/*lang=json*/
-	public const string GridJsonString = """
-		[
-			{
-				"$type": "GivenTextItem",
-				"TemplateIndex": 0,
-				"Text": "2",
-				"Cell": 1,
-				"Color": "#000000FF"
-			},
-			{
-				"$type": "GivenTextItem",
-				"TemplateIndex": 0,
-				"Text": "9",
-				"Cell": 4,
-				"Color": "#000000FF"
-			},
-			{
-				"$type": "GivenTextItem",
-				"TemplateIndex": 0,
-				"Text": "6",
-				"Cell": 7,
-				"Color": "#000000FF"
-			},
-			{
-				"$type": "GivenTextItem",
-				"TemplateIndex": 0,
-				"Text": "7",
-				"Cell": 14,
-				"Color": "#000000FF"
-			},
-			{
-				"$type": "GivenTextItem",
-				"TemplateIndex": 0,
-				"Text": "5",
-				"Cell": 20,
-				"Color": "#000000FF"
-			},
-			{
-				"$type": "GivenTextItem",
-				"TemplateIndex": 0,
-				"Text": "8",
-				"Cell": 22,
-				"Color": "#000000FF"
-			},
-			{
-				"$type": "GivenTextItem",
-				"TemplateIndex": 0,
-				"Text": "9",
-				"Cell": 26,
-				"Color": "#000000FF"
-			},
-			{
-				"$type": "GivenTextItem",
-				"TemplateIndex": 0,
-				"Text": "3",
-				"Cell": 27,
-				"Color": "#000000FF"
-			},
-			{
-				"$type": "GivenTextItem",
-				"TemplateIndex": 0,
-				"Text": "7",
-				"Cell": 31,
-				"Color": "#000000FF"
-			},
-			{
-				"$type": "GivenTextItem",
-				"TemplateIndex": 0,
-				"Text": "8",
-				"Cell": 32,
-				"Color": "#000000FF"
-			},
-			{
-				"$type": "GivenTextItem",
-				"TemplateIndex": 0,
-				"Text": "5",
-				"Cell": 34,
-				"Color": "#000000FF"
-			},
-			{
-				"$type": "GivenTextItem",
-				"TemplateIndex": 0,
-				"Text": "5",
-				"Cell": 37,
-				"Color": "#000000FF"
-			},
-			{
-				"$type": "GivenTextItem",
-				"TemplateIndex": 0,
-				"Text": "1",
-				"Cell": 38,
-				"Color": "#000000FF"
-			},
-			{
-				"$type": "GivenTextItem",
-				"TemplateIndex": 0,
-				"Text": "6",
-				"Cell": 41,
-				"Color": "#000000FF"
-			},
-			{
-				"$type": "GivenTextItem",
-				"TemplateIndex": 0,
-				"Text": "3",
-				"Cell": 42,
-				"Color": "#000000FF"
-			},
-			{
-				"$type": "GivenTextItem",
-				"TemplateIndex": 0,
-				"Text": "1",
-				"Cell": 48,
-				"Color": "#000000FF"
-			},
-			{
-				"$type": "GivenTextItem",
-				"TemplateIndex": 0,
-				"Text": "6",
-				"Cell": 53,
-				"Color": "#000000FF"
-			},
-			{
-				"$type": "GivenTextItem",
-				"TemplateIndex": 0,
-				"Text": "4",
-				"Cell": 55,
-				"Color": "#000000FF"
-			},
-			{
-				"$type": "GivenTextItem",
-				"TemplateIndex": 0,
-				"Text": "9",
-				"Cell": 59,
-				"Color": "#000000FF"
-			},
-			{
-				"$type": "GivenTextItem",
-				"TemplateIndex": 0,
-				"Text": "2",
-				"Cell": 60,
-				"Color": "#000000FF"
-			},
-			{
-				"$type": "GivenTextItem",
-				"TemplateIndex": 0,
-				"Text": "4",
-				"Cell": 66,
-				"Color": "#000000FF"
-			},
-			{
-				"$type": "GivenTextItem",
-				"TemplateIndex": 0,
-				"Text": "7",
-				"Cell": 71,
-				"Color": "#000000FF"
-			},
-			{
-				"$type": "GivenTextItem",
-				"TemplateIndex": 0,
-				"Text": "8",
-				"Cell": 72,
-				"Color": "#000000FF"
-			},
-			{
-				"$type": "GivenTextItem",
-				"TemplateIndex": 0,
-				"Text": "2",
-				"Cell": 76,
-				"Color": "#000000FF"
-			},
-			{
-				"$type": "GivenTextItem",
-				"TemplateIndex": 0,
-				"Text": "1",
-				"Cell": 79,
-				"Color": "#000000FF"
-			}
-		]
-		""";
-
-	/// <summary>
-	/// Represents options.
-	/// </summary>
-	private static readonly JsonSerializerOptions Options = new()
-	{
-		WriteIndented = true,
-		IndentCharacter = ' ',
-		IndentSize = 2,
-		IgnoreReadOnlyProperties = true
-	};
+	///// <summary>
+	///// Represents options.
+	///// </summary>
+	//private static readonly JsonSerializerOptions Options = new()
+	//{
+	//	WriteIndented = true,
+	//	IndentCharacter = ' ',
+	//	IndentSize = 2,
+	//	IgnoreReadOnlyProperties = true
+	//};
 
 
 	extension(Environment)
