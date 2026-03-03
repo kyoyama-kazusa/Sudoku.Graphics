080bc6940c4e3dc00509e4e0c969d9843e67d64b
Kyouyama Kazusa <Kazusa0805@outlook.com>
2026-03-03 13:47:45 +0800

    Update code.

--- src/Sudoku.Graphics/ComponentModel/Item.cs
+++ src/Sudoku.Graphics/ComponentModel/Item.cs
@@ -13,6 +13,7 @@
 [JsonDerivedType(typeof(CellDiceMarkItem), nameof(CellDiceMarkItem))]
 [JsonDerivedType(typeof(CellExclamationMarkItem), nameof(CellExclamationMarkItem))]
 [JsonDerivedType(typeof(CellFillItem), nameof(CellFillItem))]
+[JsonDerivedType(typeof(CellMoonPhaseMarkItem), nameof(CellMoonPhaseMarkItem))]
 [JsonDerivedType(typeof(CellPolygonMarkItem), nameof(CellPolygonMarkItem))]
 [JsonDerivedType(typeof(CellQuestionMarkItem), nameof(CellQuestionMarkItem))]
 [JsonDerivedType(typeof(CellSquareMarkItem), nameof(CellSquareMarkItem))]
