8d7f910024a4701015a93555e5b6486511e9d7ab
Kyouyama Kazusa <kazusa0805@outlook.com>
2026-04-12 16:10:05 +0800

    Update code.

--- src/Sudoku.Graphics.UI/ViewModels/AboutWindowViewModel.cs
+++ src/Sudoku.Graphics.UI/ViewModels/AboutWindowViewModel.cs
@@ -1,6 +1,11 @@
 ﻿namespace Sudoku.Graphics.UI.ViewModels;
 
-internal sealed class AboutWindowViewModel
+internal sealed class AboutWindowViewModel : ObservableObject
 {
+	public AboutWindowViewModel(ICloseService closeService) => CloseCommand = new RelayCommand(closeService.Close);
+
+
 	public Uri GitHubPage => new(LocalizationResources.ResourceManager.GetString("AboutWindow_AuthorGitHubPage", null)!);
+
+	public ICommand CloseCommand { get; }
 }
--- src/Sudoku.Graphics.UI/Views/OtherWindows/AboutWindow.xaml.cs
+++ src/Sudoku.Graphics.UI/Views/OtherWindows/AboutWindow.xaml.cs
@@ -5,10 +5,19 @@
 /// </summary>
 public partial class AboutWindow : Window
 {
+	private readonly AboutWindowViewModel _vm;
+
+
 	/// <summary>
 	/// Initializes an <see cref="AboutWindow"/> instance.
 	/// </summary>
-	public AboutWindow() => InitializeComponent();
+	public AboutWindow()
+	{
+		InitializeComponent();
+
+		_vm = new(new CloseService(this));
+		DataContext = _vm;
+	}
 
 
 	private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
@@ -16,4 +25,9 @@ public partial class AboutWindow : Window
 		Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri) { UseShellExecute = true });
 		e.Handled = true;
 	}
+}
+
+file sealed class CloseService(AboutWindow _window) : ICloseService
+{
+	public void Close() => _window.Close();
 }
