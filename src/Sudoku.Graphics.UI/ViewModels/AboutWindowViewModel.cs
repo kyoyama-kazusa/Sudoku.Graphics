ea9f92d3a6cc472de5ec8ca095dcda1e634e380c
Kyouyama Kazusa <Kazusa0805@outlook.com>
2026-04-13 16:44:44 +0800

    Update code.

--- src/Sudoku.Graphics.UI/ViewModels/AboutWindowViewModel.cs
+++ src/Sudoku.Graphics.UI/ViewModels/AboutWindowViewModel.cs
@@ -5,6 +5,7 @@ internal sealed class AboutWindowViewModel : ObservableObject
 	public AboutWindowViewModel(ICloseService closeService) => CloseCommand = new RelayCommand(closeService.Close);
 
 
+	[SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "<Pending>")]
 	public Uri GitHubPage => new(LocalizationResources.ResourceManager.GetString("AboutWindow_AuthorGitHubPage", null)!);
 
 	public ICommand CloseCommand { get; }
