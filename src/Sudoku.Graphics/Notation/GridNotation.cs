bacee2a579b7425f5914f3f04daf4d22e9c8b679
Kyouyama Kazusa <Kazusa0805@outlook.com>
2026-04-16 10:18:44 +0800

    Update code.

--- src/Sudoku.Graphics/Notation/GridNotation.cs
+++ src/Sudoku.Graphics/Notation/GridNotation.cs
@@ -15,11 +15,11 @@ public static class GridNotation
 		=> ch switch
 		{
 			>= '1' and <= '9' => ch - '1',
-			>= 'A' and <= 'Z' => 9 + (ch - 'A'),
-			>= 'a' and <= 'z' => 35 + (ch - 'a'),
-			>= '\u0391' and <= '\u03A1' => 61 + (ch - '\u0391'),
-			>= '\u03A3' and <= '\u03A9' => 61 + 17 + (ch - '\u03A3'),
-			>= '\u03B1' and <= '\u03C9' => 85 + (ch - '\u03B1'),
+			>= 'A' and <= 'Z' => 9 + ch - 'A',
+			>= 'a' and <= 'z' => 35 + ch - 'a',
+			>= '\u0391' and <= '\u03A1' => 61 + ch - '\u0391',
+			>= '\u03A3' and <= '\u03A9' => 61 + 17 + ch - '\u03A3',
+			>= '\u03B1' and <= '\u03C9' => 85 + ch - '\u03B1',
 			_ => throw new ArgumentOutOfRangeException(nameof(ch))
 		};
 
@@ -36,7 +36,7 @@ public static class GridNotation
 			< 9 => (char)('1' + index),
 			< 35 => (char)('A' + index - 9),
 			< 61 => (char)('a' + index - 35),
-			< 85 when index - 61 is var local => local < 17 ? (char)('\u0391' + local) : (char)('\u03A3' + (local - 17)),
+			< 85 when index - 61 is var local => local < 17 ? (char)('\u0391' + local) : (char)('\u03A3' + local - 17),
 			_ => (char)('\u03B1' + index - 85)
 		};
 }
