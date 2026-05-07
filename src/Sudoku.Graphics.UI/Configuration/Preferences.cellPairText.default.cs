33c88ce9a3209cfaec31c7043e62d5a7d5e5610b
Kyouyama Kazusa <Kazusa0805@outlook.com>
2026-05-07 11:21:04 +0800

    Update code.

---
+++ src/Sudoku.Graphics.UI/Configuration/Preferences.cellPairText.default.cs
@@ -0,0 +1,44 @@
+﻿namespace Sudoku.Graphics.UI.Configuration;
+
+internal partial class Preferences
+{
+	/// <summary>
+	/// Indicates default cell pair text font name.
+	/// </summary>
+	public Inherited<string> DefaultCellPairTextFontName { get; set; } = Inherited<string>.FromValue("Consolas");
+
+	/// <summary>
+	/// Indicates default cell pair text font slant.
+	/// </summary>
+	public Inherited<SKFontStyleSlant> DefaultCellPairTextFontSlant { get; set; } = Inherited<SKFontStyleSlant>.FromPropertyName(nameof(DefaultFontSlant));
+
+	/// <summary>
+	/// Indicates default cell pair text font width.
+	/// </summary>
+	public Inherited<SKFontStyleWidth> DefaultCellPairTextFontWidth { get; set; } = Inherited<SKFontStyleWidth>.FromPropertyName(nameof(DefaultFontWidth));
+
+	/// <summary>
+	/// Indicates default cell pair text font weight.
+	/// </summary>
+	public Inherited<SKFontStyleWeight> DefaultCellPairTextFontWeight { get; set; } = Inherited<SKFontStyleWeight>.FromPropertyName(nameof(DefaultFontWeight));
+
+	/// <summary>
+	/// Indicates default cell pair text font size scale, related to cell size.
+	/// </summary>
+	public Inherited<Scale> DefaultCellPairTextFontSizeScale { get; set; } = Inherited<Scale>.FromValue(0.4M);
+
+	/// <summary>
+	/// Indicates default cell pair text font color.
+	/// </summary>
+	public Inherited<SerializableColor> DefaultCellPairTextFontColor { get; set; } = Inherited<SerializableColor>.FromValue(SKColors.Black);
+
+	/// <summary>
+	/// Indicates default cell pair text cover color.
+	/// </summary>
+	public Inherited<SerializableColor> DefaultCellPairTextCoverColor { get; set; } = Inherited<SerializableColor>.FromValue(SKColors.White);
+
+	/// <summary>
+	/// Indicates default cell pair text padding.
+	/// </summary>
+	public Inherited<Thickness<float>> DefaultCellPairTextPadding { get; set; } = Inherited<Thickness<float>>.FromValue(new(6));
+}
---
+++ src/Sudoku.Graphics.UI/Configuration/Preferences.cellPairText.number.cs
@@ -0,0 +1,44 @@
+﻿namespace Sudoku.Graphics.UI.Configuration;
+
+internal partial class Preferences
+{
+	/// <summary>
+	/// Indicates cell number font name.
+	/// </summary>
+	public Inherited<string> CellNumberFontName { get; set; } = Inherited<string>.FromPropertyName(nameof(DefaultCellPairTextFontName));
+
+	/// <summary>
+	/// Indicates cell number font slant.
+	/// </summary>
+	public Inherited<SKFontStyleSlant> CellNumberFontSlant { get; set; } = Inherited<SKFontStyleSlant>.FromPropertyName(nameof(DefaultCellPairTextFontSlant));
+
+	/// <summary>
+	/// Indicates cell number font width.
+	/// </summary>
+	public Inherited<SKFontStyleWidth> CellNumberFontWidth { get; set; } = Inherited<SKFontStyleWidth>.FromPropertyName(nameof(DefaultCellPairTextFontWidth));
+
+	/// <summary>
+	/// Indicates cell number font weight.
+	/// </summary>
+	public Inherited<SKFontStyleWeight> CellNumberFontWeight { get; set; } = Inherited<SKFontStyleWeight>.FromPropertyName(nameof(DefaultCellPairTextFontWeight));
+
+	/// <summary>
+	/// Indicates cell number font size scale, related to cell size.
+	/// </summary>
+	public Inherited<Scale> CellNumberFontSizeScale { get; set; } = Inherited<Scale>.FromPropertyName(nameof(DefaultCellPairTextFontSizeScale));
+
+	/// <summary>
+	/// Indicates cell number font color.
+	/// </summary>
+	public Inherited<SerializableColor> CellNumberFontColor { get; set; } = Inherited<SerializableColor>.FromPropertyName(nameof(DefaultCellPairTextFontColor));
+
+	/// <summary>
+	/// Indicates cell number cover color.
+	/// </summary>
+	public Inherited<SerializableColor> CellNumberCoverColor { get; set; } = Inherited<SerializableColor>.FromPropertyName(nameof(DefaultCellPairTextCoverColor));
+
+	/// <summary>
+	/// Indicates cell number padding.
+	/// </summary>
+	public Inherited<Thickness<float>> CellNumberPadding { get; set; } = Inherited<Thickness<float>>.FromPropertyName(nameof(DefaultCellPairTextPadding));
+}
