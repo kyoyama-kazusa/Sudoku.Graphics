9b20ede755196c44b3d088cb5c72f8b4b00fbf05
Kyouyama Kazusa <Kazusa0805@outlook.com>
2026-05-15 11:19:11 +0800

    Update code.

--- src/Sudoku.Graphics.UI/OperationHandlers/LineSegmentOperationHandler.cs
+++ src/Sudoku.Graphics.UI/OperationHandlers/LineSegmentOperationHandler.cs
@@ -53,29 +53,7 @@ public abstract class LineSegmentOperationHandler(bool _isThick) : OperationHand
 
 
 						bool areSame(Absolute cell1, Direction4 direction1, Absolute cell2, Direction4 direction2)
-						{
-							return k(cell1, direction1) == k(cell2, direction2);
-
-
-							EdgeKey k(Absolute cell, Direction4 direction)
-							{
-								var row = cell / mapper.AbsoluteColumnsCount;
-								var column = cell % mapper.AbsoluteColumnsCount;
-								var (x1, y1, x2, y2) = direction switch
-								{
-									Direction4.Up => (column, row, column + 1, row),
-									Direction4.Right => (column + 1, row, column + 1, row + 1),
-									Direction4.Down => (column, row + 1, column + 1, row + 1),
-									Direction4.Left => (column, row, column, row + 1),
-									_ => throw new ArgumentOutOfRangeException(nameof(direction))
-								};
-								if (x1 > x2 || x1 == x2 && y1 > y2)
-								{
-									(x1, y1, x2, y2) = (x2, y2, x1, y1);
-								}
-								return new(x1, y1, x2, y2);
-							}
-						}
+							=> EdgeKey.Create(cell1, direction1, mapper) == EdgeKey.Create(cell2, direction2, mapper);
 					}
 				);
 				if (found.Length == 0)
@@ -94,12 +72,3 @@ public abstract class LineSegmentOperationHandler(bool _isThick) : OperationHand
 	protected internal sealed override bool IsAvailable(OperationHandlerContext context)
 		=> context.MouseEventArgs.ChangedButton == MouseButton.Left;
 }
-
-/// <summary>
-/// Represents a key of line segment.
-/// </summary>
-/// <param name="X1">Indicates the value X1.</param>
-/// <param name="Y1">Indicates the value Y1.</param>
-/// <param name="X2">Indicates the value X2.</param>
-/// <param name="Y2">Indicates the value Y2.</param>
-file readonly record struct EdgeKey(Absolute X1, Absolute Y1, Absolute X2, Absolute Y2);
---
+++ src/Sudoku.Graphics/Graphics/Semantic/EdgeKey.cs
@@ -0,0 +1,41 @@
+﻿namespace Sudoku.Graphics.Semantic;
+
+/// <summary>
+/// Represents a key of an edge (line segment).
+/// </summary>
+/// <param name="Cell1Row">Indicates the value Y1.</param>
+/// <param name="Cell1Column">Indicates the value X1.</param>
+/// <param name="Cell2Row">Indicates the value Y2.</param>
+/// <param name="Cell2Column">Indicates the value X2.</param>
+public readonly record struct EdgeKey(Absolute Cell1Row, Absolute Cell1Column, Absolute Cell2Row, Absolute Cell2Column) :
+	IEqualityOperators<EdgeKey, EdgeKey, bool>
+{
+	/// <summary>
+	/// Creates an <see cref="EdgeKey"/> instance via the specified cell and direction.
+	/// </summary>
+	/// <param name="cell">The cell.</param>
+	/// <param name="direction">The direction.</param>
+	/// <param name="mapper">The point mapper instance.</param>
+	/// <returns>An <see cref="EdgeKey"/> instance.</returns>
+	/// <exception cref="ArgumentOutOfRangeException">Throws when <paramref name="direction"/> is not defined.</exception>
+	public static EdgeKey Create(Absolute cell, Direction4 direction, PointMapper mapper)
+	{
+		var row = cell / mapper.AbsoluteColumnsCount;
+		var column = cell % mapper.AbsoluteColumnsCount;
+#pragma warning disable format
+		var (row1, column1, row2, column2) = direction switch
+		{
+			Direction4.Up    => (row    , column    , row    , column + 1),
+			Direction4.Right => (row    , column + 1, row + 1, column + 1),
+			Direction4.Down  => (row + 1, column    , row + 1, column + 1),
+			Direction4.Left  => (row    , column    , row + 1, column    ),
+			_                => throw new ArgumentOutOfRangeException(nameof(direction))
+		};
+#pragma warning restore format
+		if (column1 > column2 || column1 == column2 && row1 > row2)
+		{
+			(column1, row1, column2, row2) = (column2, row2, column1, row1);
+		}
+		return new(row1, column1, row2, column2);
+	}
+}
