f0a8764aa22c65540ced4afb88a2537d3fb5c3d4
Kyouyama Kazusa <kazusa0805@outlook.com>
2026-05-17 22:29:19 +0800

    Update version.

--- src/System/Runtime/CompilerServices/UnionAttribute.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace System.Runtime.CompilerServices;
-
-/// <summary>
-/// Represents a type that is a union or a union-like type.
-/// </summary>
-[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, Inherited = false)]
-public sealed class UnionAttribute : Attribute;
